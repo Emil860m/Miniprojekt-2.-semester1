0737d859d8a98cbadcef415bb9d03133b416ebae
Emil860m <fenger.emil@gmail.com>
2018-03-12 09:35:04 +0100

    Arraylists

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,12 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class HomeController {
+    private ArrayList<VentelisteBørn> vBoern = new ArrayList();
+    private ArrayList<Boern> boern = new ArrayList();
+    private ArrayList<Ansat> ansat = new ArrayList();
+
     @GetMapping("/")
     public String index(Model model){
 
-        //model.addAttribute();
+        model.addAttribute("vBoern", vBoern);
+        model.addAttribute("boern", boern);
+        model.addAttribute("ansat", ansat);
         return "index";
     }
 

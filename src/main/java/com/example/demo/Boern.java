62a26ece098e4f7859793000eaca73b89fe098f1
Emil860m <fenger.emil@gmail.com>
2018-03-13 10:13:04 +0100

    Telefonliste

--- src/main/java/com/example/demo/Boern.java
+++ src/main/java/com/example/demo/Boern.java
@@ -1,6 +1,19 @@
 package com.example.demo;
 
 public class Boern {
+    private String Navn;
+    private String Efternavn;
+    private String Adresse;
+    private int Telefonnummer;
+    private String Kontaktperson;
+    private String Kontaktperson2;
 
-
+    public Boern(String navn, String efternavn, String adresse, int telefonnummer, String kontaktperson, String kontaktperson2) {
+        Navn = navn;
+        Efternavn = efternavn;
+        Adresse = adresse;
+        Telefonnummer = telefonnummer;
+        Kontaktperson = kontaktperson;
+        Kontaktperson2 = kontaktperson2;
+    }
 }
--- src/main/java/com/example/demo/VentelisteBørn.java
+++ src/main/java/com/example/demo/VentelisteBørn.java
@@ -6,16 +6,20 @@ public class VentelisteBørn {
     private String Adresse;
     private int Telefonnummer;
     private String Kontaktperson;
+    private String Kontaktperson2;
+    private int id;
 
-    public VentelisteBørn(String navn, String efternavn, String adresse, int telefonnummer, String kontaktperson) {
+    public VentelisteBørn(String navn, String efternavn, String adresse, int telefonnummer, String kontaktperson, String kontaktperson2, int id) {
         Navn = navn;
         Efternavn = efternavn;
         Adresse = adresse;
         Telefonnummer = telefonnummer;
         Kontaktperson = kontaktperson;
+        Kontaktperson2 = kontaktperson2;
+        this.id = id;
     }
 
-        public String getNavn(){
+    public String getNavn(){
             return Navn;
         }
 
@@ -55,5 +59,20 @@ public class VentelisteBørn {
         Kontaktperson = kontaktperson;
         }
 
+    public String getKontaktperson2() {
+        return Kontaktperson2;
     }
+
+    public void setKontaktperson2(String kontaktperson2) {
+        Kontaktperson2 = kontaktperson2;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
 

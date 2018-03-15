fee4f03f564bd09316558f999455e6445f644b50
michjens <michael.diksi@hotmail.com>
2018-03-15 11:40:02 +0100

    cddd

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -14,48 +14,34 @@ import java.util.ArrayList;
 @Controller
 public class HomeController {
     private ArrayList<VentelisteBørn> vBoern = new ArrayList();
-    private ArrayList<Boern> boern = new ArrayList();
-    private ArrayList<Ansat> ansat = new ArrayList();
-
+  //  private ArrayList<Boern> boern = new ArrayList();
+ //   private ArrayList<Ansat> ansat = new ArrayList();
+    int barnID = 0;
     @GetMapping("/")
     public String index(Model model){
-    
+
         model.addAttribute("vBoern", vBoern);
         //model.addAttribute("boern", boern);
         //model.addAttribute("ansat", ansat);
-        vBoern.add(new VentelisteBørn("Emil Fenger", "Lygten 37", 12345678, "Micheal Jensen", "Lasse Kjær", 1, true));
+        //vBoern.add(new VentelisteBørn("Emil Fenger", "Lygten 37", 12345678, "Micheal Jensen", "Lasse Kjær", 1, true));
         return "venteliste";
     }
-    /*
-    @GetMapping("/rediger")
-    public String edit(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
-        int selectedStudentID = 0;
-        for (int i = 0; i < vBoern.size(); i++) {
-            if (vBoern.get(i).getId() == id) {
-                selectedStudentID = i;
-            }
-        }
 
-        model.addAttribute("boern", vBoern.get(selectedStudentID));
-        return "rediger";
+    @GetMapping("/edit")
+    public String edit(@RequestParam(value = "id", defaultValue = "1") int id, Model model) {
+        if (model != null) {
+            model.addAttribute("vBoern", vBoern.get(id - 1));
+        }
+        barnID = id;
+        return "edit";
+    }
+    @PostMapping("/edit")
+    public String edit(@ModelAttribute VentelisteBørn boern) {
+        boern.setId(barnID);
+        vBoern.set(barnID - 1,boern);
+        return "redirect:/";
     }
 
-    @PostMapping("/rediger")
-    public String rediger(@ModelAttribute VentelisteBørn boern) {
-        for (int i = 0; i < vBoern.size(); i++) {
-            if (boern.getId() == vBoern.get(i).getId()) {
-                // DEBUG -- Print the id of student to delete System.out.println(students.get(i).getStudentID());
-                vBoern.set(i,boern);
-                break;
-            } else {
-                //DEBUG -- Print while not finding the correct ID System.out.println("Could not find it::");
-            }
-        }
-       /* int index = student.getStudentID();
-        student.setStudentID(index);
-        students.set(index - 1, student);
-        return "redirect:/";
-    }*/
 
     @GetMapping("/create")
     public String create(Model model) {
